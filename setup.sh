c77463b272fed9de5d99ee045deb3cb3c37c0833
Niclas Grande <ngrande@vwd.com>
2019-02-20 14:31:00 +0100

    Add ssh config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # create symlinks from all to my dotfiles
+echo "Installing dot files..."
 for file in "$(pwd)/dot"/*
 do
 	dot_name=.$(basename $file)
@@ -10,3 +11,14 @@ do
 	ln -sf $file ~/$dot_name
 done
 
+echo "Installing ssh configs..."
+for file in "$(pwd)/ssh"/*
+do
+	config_name=$(basename $file)
+	echo "Creating symlink ~/.ssh/$config_name -> $file"
+	echo "Confirm with [ENTER]"
+	read
+	ln -sf $file ~/.ssh/$config_name
+done
+
+echo "Finished."
