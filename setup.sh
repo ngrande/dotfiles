c9dab6bc0cfda2676fd43c11ad664255bd97e5a2
Niclas Grande <ngrande@vwd.com>
2019-02-21 11:44:17 +0100

    Remove ssh config

--- setup.sh
+++ setup.sh
@@ -11,16 +11,6 @@ do
 	ln -sf $file ~/$dot_name
 done
 
-echo "Installing ssh configs..."
-for file in "$(pwd)/ssh"/*
-do
-	config_name=$(basename $file)
-	echo "Creating symlink ~/.ssh/$config_name -> $file"
-	echo "Confirm with [ENTER]"
-	read
-	ln -sf $file ~/.ssh/$config_name
-done
-
 echo "Installing pacman hooks..."
 echo "This requires sudo!"
 echo "Confirm that you can read... [ENTER]"
